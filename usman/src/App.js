5d5fe900569159b5d4d5fa493aef9a1561ebc0df
Usman Muavia <97330175+its-pz-mir@users.noreply.github.com>
2024-02-27 15:14:00 +0000

    2nd Commit

--- usman/src/App.js
+++ usman/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Root Router</div>
+    },
+    {
+        path: "/about",
+        element: <div>About Router</div>
+    }
+])
 
 export default function App() {
     return (
-        <div>
-            App
-        </div>
+        <main>
+            <RouterProvider router={router}></RouterProvider>
+        </main>
     )
 }
